3a0684709ac1e140fb69d772a7822ff7d3d4722c
Anas <anas.ergeshov.98@gmail.com>
2021-06-12 17:34:32 +0600

    asdasd

--- course/views.py
+++ course/views.py
@@ -30,7 +30,20 @@ def group_list(request):
     return render(request, 'course/groups.html', context=my_context)
 
 
+def group_detail(request, group_id):
+    group = Group.objects.get(id=group_id)
+    students = Student.objects.filter(group=group)
+    context = {'group': group, 'students': students}
+    return render(request, 'course/group-detail.html', context=context)
+
+
 def student_list(request):
     students = Student.objects.all()
     my_context = {'students': students}
     return render(request, 'course/students.html', context=my_context)
+
+
+def student_detail(request, student_id):
+    student = Student.objects.get(id=student_id)
+    my_context = {'student': student}
+    return render(request, 'course/student-detail.html', context=my_context)
