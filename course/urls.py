6fa51dc6595b50ad90109227d94a483e0b2ca986
Anas <anas.ergeshov.98@gmail.com>
2021-06-14 21:06:10 +0600

    features/create object

--- course/urls.py
+++ course/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from course.views import branch_detail, group_detail, group_list, my_main_page, branches_list, student_detail, student_list
+from course import views
 
 urlpatterns = [
-    path('', my_main_page, name='my_main_page'),
-    path('branches/', branches_list, name='branches_list'),
-    path('branches/<int:branch_id>/', branch_detail, name='branch_detail'),
-    path('groups/', group_list, name='group_list'),
-    path('groups/<int:group_id>/', group_detail, name='group_detail'),
-    path('students/', student_list, name='student_list'),
-    path('students/<int:student_id>/', student_detail, name='student_detail')
+    path('', views.my_main_page, name='my_main_page'),
+    path('branches/', views.branches_list, name='branches_list'),
+    path('branches/create/', views.branch_create, name='branch_create'),
+    path('branches/<int:branch_id>/', views.branch_detail, name='branch_detail'),
+    path('groups/', views.group_list, name='group_list'),
+    path('groups/<int:group_id>/', views.group_detail, name='group_detail'),
+    path('students/', views.student_list, name='student_list'),
+    path('students/<int:student_id>/', views.student_detail, name='student_detail')
 ]
--- course/views.py
+++ course/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from course.forms import BranchForm
+from django.http.response import Http404
+from django.shortcuts import get_object_or_404, redirect, render
 from django.http import HttpResponse
 from .models import Branch, Group, Student
 
@@ -18,10 +20,22 @@ def branches_list(request):
 
 
 def branch_detail(request, branch_id):
-    branch = Branch.objects.get(id=branch_id)
+    branch = get_object_or_404(Branch, pk=branch_id)
     groups = Group.objects.filter(branch=branch)
     context = {'branch': branch, 'groups': groups}
     return render(request, 'course/branch-detail.html', context=context)
+
+
+def branch_create(request):
+    if request.method == "POST":
+        form = BranchForm(request.POST)
+        if form.is_valid():
+            branch = form.save()
+            return redirect('branch_detail', branch_id=branch.id)
+    else:
+        form = BranchForm()
+
+    return render(request, 'course/branch-create.html', {'form': form})
 
 
 def group_list(request):
