68d80b22a68183b7a8c4c1e1480da1f30ee3cce4
Anas <anas.ergeshov.98@gmail.com>
2021-06-16 20:49:56 +0600

    learn imagefield

--- course/models.py
+++ course/models.py
@@ -8,6 +8,7 @@ class Branch(models.Model):
     
     name = models.CharField(max_length=100, null=False)
     address = models.CharField(max_length=300, null=True)
+    photo = models.ImageField(upload_to='branches/', null=True, blank=True)
 
     def __str__(self):
         return self.name
